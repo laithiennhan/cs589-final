c7cff030a9097439f70400e8fa8a2a8dd992e6a7
laithiennhan <ntlai@umass.edu>
2024-05-03 23:47:03 -0400

    refractor knn

--- knn.py
+++ knn.py
@@ -4,167 +4,65 @@ import math
 import numpy as np
 from collections import defaultdict
 from matplotlib import pyplot as plt
-from sklearn import datasets
+from utils import convert_to_float, load_data, normalize, split_folds, split_train_test, eval, get_majority
+import sys
 
 
 def euclidean_distance(r1, r2):
-    return np.sqrt(np.sum((np.array(r1[:-1]) - np.array(r2[:-1])) ** 2))
+    r1 = np.array(r1, dtype=float)
+    r2 = np.array(r2, dtype=float)
+    return np.sqrt(np.sum((r1 - r2) ** 2))
 
 
-def find_k_nearest_neighbors(r, k, training_set):
-    distances = np.array([euclidean_distance(r, row) for row in training_set])
+def find_k_nearest_neighbors(r, k, training_X, training_Y):
+    distances = np.array([])
+    for i in range(len(training_X)):
+        distances = np.append(distances, euclidean_distance(r, training_X[i]))
     nearest_indices = np.argsort(distances)[:k]
-    return [training_set[i] for i in nearest_indices]
+
+    return [training_Y[i] for i in nearest_indices]
 
 
-# Ordinal encoding for categorical features
-
-def ordinal_encoding(categorical_indices, dataset):
-    dataset_copy = dataset.copy()
-    for i in categorical_indices:
-        curr_val = 0
-        value_map = dict()
-        for row in dataset_copy:
-            if row[i] not in value_map.keys():
-                row[i] = curr_val
-                value_map[row[i]] = curr_val
-                curr_val += 1
-            else:
-                row[i] = value_map[row[i]]
-    print(dataset_copy)
-    return dataset_copy
-
-
-def convert_to_float(item):
-    try:
-        return float(item)
-    except ValueError:
-        return item
+def train_knn(k_val, data, is_normalize):
+    # correct_predictions = 0
+    # tp = defaultdict(int)
+    # fp = defaultdict(int)
+    # fn = defaultdict(int)
+    (training_X, testing_X, training_y, testing_y) = data
+    y_pred = []
+    y_true = []
+    training_X = np.array(training_X, dtype=float)
+    testing_X = np.array(testing_X, dtype=float)
+    if is_normalize:
+        normalize(training_X)
+        normalize(testing_X)
+    for i in range(len(testing_X)):
+        neighbors = find_k_nearest_neighbors(
+            testing_X[i], k_val, training_X, training_y)
+        predicted_class = get_majority(neighbors)
+        y_pred.append(predicted_class)
+        y_true.append(testing_y[i])
+    return eval(y_true, y_pred)
 
 
-def load_data(filename):
-    data = []
-    labels = []
-    if (filename == 'loan.csv'):
-        with open('datasets/loan.csv', 'r') as file:
-            csvFile = csv.reader(file)
-            next(csvFile)  # Skip header
-            for line in csvFile:
-                features = [convert_to_float(item) for item in line[:-1]]
-                label = line[-1]
-                data.append(features + [label])
-
-        data = [row[1:] for row in data]
-        return ordinal_encoding([0, 1, 2, 3, 4, 9, 10], data)
-    elif (filename == 'titanic.csv'):
-        with open('datasets/titanic.csv', 'r') as file:
-            csvFile = csv.reader(file)
-            next(csvFile)  # Skip header
-            for line in csvFile:
-                features = [convert_to_float(item) for item in line[1:]]
-                label = line[0]
-                data.append(features + [label])
-        data = [row[:1] + row[2:] for row in data]
-        return ordinal_encoding([1], data)
-    else:
-        with open(f'datasets/{filename}', 'r') as file:
-            csvFile = csv.reader(file)
-            next(csvFile)  # Skip header
-            for line in csvFile:
-                features = [convert_to_float(item) for item in line[:-1]]
-                label = float(line[-1])
-                data.append(features + [label])
-
-        return ordinal_encoding([], data)
-
-
-def create_stratified_folds(data, k):
-    class_dict = defaultdict(list)
-    for item in data:
-        class_dict[item[-1]].append(item)
-
-    folds = [[] for _ in range(k)]
-    for _, items in class_dict.items():
-        np.random.shuffle(items)
-        for idx, item in enumerate(items):
-            folds[idx % k].append(item)
-
-    return folds
-
-
-def normalize_datasets(training_set, testing_set):
-    new_training = training_set.copy()
-    new_testing = testing_set.copy()
-    min_col = [0] * (len(training_set[0]) - 1)
-    max_col = [0] * (len(training_set[0]) - 1)
-    for i in range(len(min_col)):
-        min_col[i] = min([row[i] for row in training_set])
-        max_col[i] = max([row[i] for row in training_set])
-    for i in range(len(training_set)):
-        for j in range(len(training_set[0]) - 1):
-            new_training[i][j] = (new_training[i][j] - min_col[j]
-                                  ) / (max_col[j] - min_col[j]) if max_col[j] - min_col[j] != 0 else 0
-    for i in range(len(testing_set)):
-        for j in range(len(testing_set[0]) - 1):
-            new_testing[i][j] = (new_testing[i][j] - min_col[j]
-                                 ) / (max_col[j] - min_col[j]) if max_col[j] - min_col[j] != 0 else 0
-    return new_training, new_testing
-
-
-def train_knn(k, training_set, test_set, normalize):
-    correct_predictions = 0
-    tp = defaultdict(int)
-    fp = defaultdict(int)
-    fn = defaultdict(int)
-
-    # for label in labels:
-    #     tp[label] = 0
-    #     fp[label] = 0
-    #     tn[label] = 0
-    #     fn[label] = 0
-
-    if normalize:
-        training_set, test_set = normalize_datasets(training_set, test_set)
-    for test_instance in test_set:
-        neighbors = find_k_nearest_neighbors(test_instance, k, training_set)
-        predicted_class = max(set([neighbor[-1] for neighbor in neighbors]),
-                              key=[neighbor[-1] for neighbor in neighbors].count)
-        if predicted_class == test_instance[-1]:
-            correct_predictions += 1
-            tp[predicted_class] += 1
-        else:
-            fp[predicted_class] += 1
-            fn[test_instance[-1]] += 1
-
-    accuracy = correct_predictions / len(test_set)
-    f1_scores = []
-    # Confusion matrix
-    labels = set(tp.keys()).union(
-        fp.keys()).union(fp.keys())
-    for label in labels:
-        f1_scores.append(tp[label] / (tp[label] + 1 /
-                         2 * (fp[label] + fn[label])))
-
-    return accuracy, np.mean(f1_scores)
-
-
-def parallel_knn(k_values, folds, k, normalize=False):
+def parallel_knn(k_values, X, y, k, is_normalize=False):
     results = {k_val: {'accuracies': [], 'f1_scores': []}
                for k_val in k_values}
 
     for k_val in k_values:
+        folds = split_folds(y, k)
+
         with ThreadPoolExecutor(max_workers=5) as executor:
-            future_to_fold = {executor.submit(train_knn, k_val, [item for j in range(
-                k) if j != i for item in folds[j]], folds[i], normalize): i for i in range(k)}
+            future_to_fold = {executor.submit(
+                train_knn, k_val, split_train_test(X, y, folds, i), is_normalize): i for i in range(k)}
 
             for future in as_completed(future_to_fold):
                 fold_index = future_to_fold[future]
-                try:
-                    accuracy, f1_score = future.result()
-                    results[k_val]['accuracies'].append(accuracy)
-                    results[k_val]['f1_scores'].append(f1_score)
-                except Exception as e:
-                    print(f'Fold {fold_index} generated an exception: {e}')
+
+                train_results = future.result()
+                results[k_val]['accuracies'].append(
+                    train_results["accuracy"])
+                results[k_val]['f1_scores'].append(train_results["f1"])
 
         # Calculate mean accuracy for this k value
         results[k_val]['mean_accuracy'] = np.mean(results[k_val]['accuracies'])
@@ -173,40 +71,42 @@ def parallel_knn(k_values, folds, k, normalize=False):
     return results
 
 
-filename = 'loan.csv'
+def main(filename, is_normalize=False):
 
-data = []
-labels = []
+    X, y = load_data(filename, encode=True)
+    k = 10
+    k_values = range(1, 52, 2)
+    results = parallel_knn(k_values, X, y, k, is_normalize)
 
-if (filename == "titanic.csv"):
-    digits = datasets.load_digits()
-    data = digits.data
+    ks = list(k_values)
 
-else:
-    data = load_data(filename)
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(ks, [results[k]['mean_accuracy'] for k in ks], marker='o')
+    plt.xlabel('Number of Neighbors: k')
+    plt.ylabel('Cross-validated Accuracy')
+    plt.title(f'KNN Accuracy {filename}')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(ks, [results[k]['mean_f1_score'] for k in ks], marker='o')
+    plt.xlabel('Number of Neighbors: k')
+    plt.ylabel('Cross-validated F1 Score')
+    plt.title(f'KNN F1 Score {filename}')
+
+    plt.tight_layout()
+    plt.savefig(f'./figures/knn_{filename}.png')
 
 
-k = 10
-folds = create_stratified_folds(data, k)
-
-k_values = range(1, 40, 2)
-results = parallel_knn(k_values, folds, k, normalize=True)
-
-ks = list(k_values)
-
-
-plt.figure(figsize=(15, 5))
-plt.subplot(1, 2, 1)
-plt.plot(ks, [results[k]['mean_accuracy'] for k in ks], marker='o')
-plt.xlabel('Number of Neighbors: k')
-plt.ylabel('Cross-validated Accuracy')
-plt.title(f'KNN Accuracy {filename}')
-
-plt.subplot(1, 2, 2)
-plt.plot(ks, [results[k]['mean_f1_score'] for k in ks], marker='o')
-plt.xlabel('Number of Neighbors: k')
-plt.ylabel('Cross-validated F1 Score')
-plt.title(f'KNN F1 Score {filename}')
-
-plt.tight_layout()
-plt.savefig(f'./figures/knn_{filename}.png')
+if __name__ == '__main__':
+    if len(sys.argv) not in [2, 3]:
+        print("Usage: python main.py <filename> (optional: use flag --normalize to enable data normalization)")
+        sys.exit(1)
+    if len(sys.argv) == 3:
+        if sys.argv[2] == '--normalize':
+            main(sys.argv[1], is_normalize=True)
+        else:
+            print(
+                "Usage: python main.py <filename> (optional: use flag --normalize to enable data normalization)")
+        sys.exit(1)
+    else:
+        main(sys.argv[1])
